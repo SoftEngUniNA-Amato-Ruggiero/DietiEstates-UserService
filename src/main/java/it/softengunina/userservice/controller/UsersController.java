41eb20d44c224debe8c3bd40ff13e80f4e87e645
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-02 10:30:46 +0200

    getUsers allows filtering by email as query parameter

--- src/main/java/it/softengunina/userservice/controller/UsersController.java
+++ src/main/java/it/softengunina/userservice/controller/UsersController.java
@@ -2,16 +2,19 @@ package it.softengunina.userservice.controller;
 
 import it.softengunina.userservice.model.User;
 import it.softengunina.userservice.repository.UserRepository;
+import jakarta.validation.constraints.Email;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping()
 public class UsersController {
     private static final String NOT_FOUND_MESSAGE = "User not found with id ";
+    private static final String EMAIL_NOT_FOUND_MESSAGE = "User not found with email ";
     private final UserRepository<User> repository;
 
     public UsersController(UserRepository<User> repository) {
@@ -19,8 +22,14 @@ public class UsersController {
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return repository.findAll();
+    public List<User> getUsers(@Email @RequestParam(value = "email", required = false) String email) {
+        if (email != null) {
+            Optional<User> optionalUser = repository.findByEmail(email);
+            User user = optionalUser.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, EMAIL_NOT_FOUND_MESSAGE + email));
+            return List.of(user);
+        } else {
+            return repository.findAll();
+        }
     }
 
     @GetMapping("/{id}")
