e681dbb8accf8a5fec81128e0e1b3730c1b6d7fa
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-04 18:04:12 +0200

    added UserRequest DTO

---
+++ src/main/java/it/softengunina/userservice/dto/UserRequest.java
@@ -0,0 +1,19 @@
+package it.softengunina.userservice.dto;
+
+import it.softengunina.userservice.model.PersonInfo;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserRequest {
+    @NotBlank
+    private String email;
+
+    @NotNull
+    @Valid
+    private PersonInfo info;
+}
