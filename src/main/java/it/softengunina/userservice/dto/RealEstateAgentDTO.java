af54a7abaad2c993bcc9b977aed43addc6aa769c
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-05 10:30:44 +0200

    better agent DTO

--- src/main/java/it/softengunina/userservice/dto/RealEstateAgentDTO.java
+++ src/main/java/it/softengunina/userservice/dto/RealEstateAgentDTO.java
@@ -1,22 +1,13 @@
 package it.softengunina.userservice.dto;
 
-import it.softengunina.userservice.model.PersonInfo;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class RealEstateAgentDTO {
-    @NotNull
-    @Valid
+    @NotBlank
+    @Email
     private String email;
-
-    @NotNull
-    @Valid
-    private PersonInfo info;
-
-    @NotNull
-    private Long agencyId;
 }
